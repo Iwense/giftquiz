f6046be66095966e96093f4a2409932668b6663a
alexander.grishin <alexander.grishin@tunneltech.eu>
2021-05-08 14:50:39 +0300

    Updates

---
+++ precache-manifest.3a8e49118851114c22e07e299e039a7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4154ddc13ccae57bfce062bc6ac9675",
+    "url": "/giftquiz/index.html"
+  },
+  {
+    "revision": "795d20738320a871c912",
+    "url": "/giftquiz/static/css/main.41afd4d1.chunk.css"
+  },
+  {
+    "revision": "afdc11e131496a857eee",
+    "url": "/giftquiz/static/js/2.2ba35895.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/giftquiz/static/js/2.2ba35895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "795d20738320a871c912",
+    "url": "/giftquiz/static/js/main.074dd296.chunk.js"
+  },
+  {
+    "revision": "6e22dd467e3c7b184635",
+    "url": "/giftquiz/static/js/runtime-main.978c7548.js"
+  }
+]);
